dbc8491b7ffb31a88ef8decb29b7c3f5359d6bb1
onurd <106928484+onurdzd@users.noreply.github.com>
2023-08-02 07:53:33 +0300

    solved

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,18 @@
+import ProductForSaleFile.Bread;
+import ProductForSaleFile.Chocolate;
+import ProductForSaleFile.Coke;
+import ProductForSaleFile.ProductForSale;
+
+import java.util.Arrays;
+
 public class Store {
     public static void main(String[] args) {
+        ProductForSale[] products= {new Chocolate("çikolata",50,"bitter çikolata",false),new Coke("kola",20,"kokakola","siyah"),new Bread("ekmek",10,"ekmek",3)};
 
-        listProducts();
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        Arrays.stream(products).forEach(product -> product.showDetails());
     }
 }
